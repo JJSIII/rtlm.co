3c4747821f9515373c84f766259b54733fb86748
Jackson Stephens <filthee@gmail.com>
2013-09-16 15:46:26 -0400

    init

---
+++ app/scripts/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+$(document).ready(function() {
+
+	// video background
+	$('.panel-home').videoBG({
+		autoplay: true,
+		position: 'absolute',
+		zIndex: 0,
+		mp4: 'videos/jellyfish.mp4',
+		ogv: 'videos/jellyfish.ogv',
+		webm: 'videos/jellyfish.webm',
+		poster: 'images/<%= yeoman.dist %>jellyfish.jpg',
+		fullscreen: true,
+		scale: false
+	});
+
+	// mute video
+	$('video').prop('muted', true);
+
+});
